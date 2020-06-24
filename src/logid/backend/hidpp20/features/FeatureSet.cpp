7ae46c938d983878ed21ce75ca8496f50e573e01
pixl <pixlone@protonmail.com>
2020-06-24 02:33:26 -0400

    Implement FeatureSet feature

---
+++ src/logid/backend/hidpp20/features/FeatureSet.cpp
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2019-2020 PixlOne
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+#include "FeatureSet.h"
+
+using namespace logid::backend::hidpp20;
+
+FeatureSet::FeatureSet(Device *device) : Feature(device, ID)
+{
+}
+
+uint8_t FeatureSet::getFeatureCount()
+{
+    std::vector<uint8_t> params(0);
+    auto response = callFunction(GetFeatureCount, params);
+    return response[0];
+}
+
+uint16_t FeatureSet::getFeature(uint8_t feature_index)
+{
+    std::vector<uint8_t> params(1);
+    params[0] = feature_index;
+    auto response = callFunction(GetFeature, params);
+
+    uint16_t feature_id = (response[0] << 8);
+    feature_id |= response[1];
+    return feature_id;
+}
+
+std::map<uint8_t, uint16_t> FeatureSet::getFeatures()
+{
+    uint8_t feature_count = getFeatureCount();
+    std::map<uint8_t, uint16_t> features;
+    for(uint8_t i = 0; i < feature_count; i++)
+        features[i] = getFeature(i);
+    return features;
+}
