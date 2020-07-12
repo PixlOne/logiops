a8dd3ba71b4c0b6e698acadd5b13992c67afef9b
pixl <pixlone@protonmail.com>
2020-07-11 23:21:15 -0400

    Add NoPress gesture mode

---
+++ src/logid/actions/gesture/NullGesture.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2019-2020 PixlOne
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+#ifndef LOGID_ACTION_NULLGESTURE_H
+#define LOGID_ACTION_NULLGESTURE_H
+
+#include "Gesture.h"
+
+namespace logid {
+namespace actions
+{
+    class NullGesture : public Gesture
+    {
+    public:
+        NullGesture(Device* device, libconfig::Setting& setting);
+
+        virtual void press();
+        virtual void release(bool primary=false);
+        virtual void move(int16_t axis);
+
+        virtual bool metThreshold() const;
+    protected:
+        int16_t _axis;
+        Gesture::Config _config;
+    };
+}}
+
+#endif //LOGID_ACTION_NULLGESTURE_H
