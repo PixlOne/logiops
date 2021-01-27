8c6509f313b90ca54e0b2bb5b2ccc078f462d7fa
zig <coderxian@gmail.com>
2021-01-27 03:16:44 -0600

    Continues uninstall attempt even after one part fails

--- uninstall.sh
+++ uninstall.sh
@@ -4,8 +4,8 @@ set -e
 
 uninstall_logid() {
     if [ 1 -gt ${#1} ]; then
-        printf '%s\n' "No manifest_path detected for uninstall"
-        exit 1
+        printf '%s\n' "No manifest_path detected for uninstall. Skipping..."
+        return
     fi
     local manifest_path="$1"
     echo "manifest_path is $manifest_path"
@@ -20,19 +20,19 @@ uninstall_logid() {
 get_manifest_path() {
     local manifest_path=$(find . -name 'install-manifest.txt' -type f -print)
     if [ 1 -lt ${#manifest_path} ]; then
-        printf '%s\n' "This repo has not been built yet. Please build it to generate install-manifest.txt"
-        exit 1
+        printf '%s\n' "This repo has not been built yet. Please build it to generate install-manifest.txt. Continuing..."
+        return
     fi
     echo "$manifest_path"
 }
 
 disable_daemon() {
-    systemctl status logid 2>&1 /dev/null
+    systemctl status logid 2> /dev/null
     if [ 0 -ne $? ]; then
         printf '%s\n' "Unable to detect logid daemon in systemctl. Exiting..."
         exit 1
     fi
-    sudo systemctl disable logid
+    sudo systemctl disable logid 2> /dev/null
 }
 
 # MAIN
@@ -42,6 +42,6 @@ manifest_path=$(get_manifest_path)
 uninstall_logid "$manifest_path"
 disable_daemon
 
-printf '%s\n' "Completed uninstall successfully"
+printf '%s\n' "Completed uninstall"
 
 
